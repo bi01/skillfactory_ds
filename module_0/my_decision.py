7b6be378901fc7c5d19a61aeb9d86e5393732dc5
Oleg Biryukov <oleg.biruykov@gmail.com>
2020-09-10 23:11:51 +0300

    Little refactoring
     - use map function for calculate scoring

--- module_0/my_decision.py
+++ module_0/my_decision.py
@@ -66,16 +66,13 @@ def score_game(game_core) -> int:
     np.random.seed(1)
 
     random_array = np.random.randint(1, 101, size=1000)
-
-    count_ls = [game_core(item) for item in random_array]
-    score = int(np.mean(count_ls))
+    score = int(np.mean(list(map(game_core, random_array))))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
 
     return score
 
 
 if __name__ == "__main__":
-
     # Проверяем
     score_game(game_core_v2)
     score_game(game_core_v1)
